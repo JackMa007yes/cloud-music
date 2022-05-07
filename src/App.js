60ec57e5f767c9abd75a6ad8199900f93bade955
wb_heguilin <wb_heguilin@meituan.com>
2022-05-07 20:10:10 +0800

    打包优化

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React, { memo } from 'react'
-import { renderRoutes } from 'react-router-config'
-import routes from '@/router'
-import store from './store'
-import { Provider } from 'react-redux'
-import AppHeader from '@/components/app-header'
-import AppFooter from '@/components/app-footer'
-import PlayerBar from '@/pages/player/app-player-bar'
-import { HashRouter } from 'react-router-dom'
+import React, { memo, Suspense } from "react";
+import { renderRoutes } from "react-router-config";
+import routes from "@/router";
+import store from "./store";
+import { Provider } from "react-redux";
+import AppHeader from "@/components/app-header";
+import AppFooter from "@/components/app-footer";
+import PlayerBar from "@/pages/player/app-player-bar";
+import { HashRouter } from "react-router-dom";
 
 const App = memo(() => {
   return (
     <Provider store={store}>
       <HashRouter>
         <AppHeader />
-        {renderRoutes(routes)}
+        <Suspense fallback={<div>page loading</div>}>
+          {renderRoutes(routes)}
+        </Suspense>
         <AppFooter />
       </HashRouter>
       <PlayerBar />
     </Provider>
-  )
-})
+  );
+});
 
-export default App
+export default App;
--- src/router/index.js
+++ src/router/index.js
@@ -1,86 +1,73 @@
-import Discover from '@/pages/discover'
-import { Redirect } from 'react-router-dom'
-import React from 'react'
+import Discover from "@/pages/discover";
+import { Redirect } from "react-router-dom";
+import React from "react";
 
-import Friends from '@/pages/friends'
-import Mine from '@/pages/mine'
-import Recommend from "../pages/discover/c-pages/recommend"
-import Ranking from "../pages/discover/c-pages/ranking"
-import Songs from "../pages/discover/c-pages/songs"
-import Djradio from "../pages/discover/c-pages/djradio"
-import Artist from "../pages/discover/c-pages/artist"
-import Album from "../pages/discover/c-pages/album"
-import Player from '../pages/player'
-
-
-// const Recommend = React.lazy(_ => import("../pages/discover/c-pages/recommend"));
-// const Ranking = React.lazy(_ => import("../pages/discover/c-pages/ranking"));
-// const Songs = React.lazy(_ => import("../pages/discover/c-pages/songs"));
-// const Djradio = React.lazy(_ => import("../pages/discover/c-pages/djradio"));
-// const Artist = React.lazy(_ => import("../pages/discover/c-pages/artist"));
-// const Album = React.lazy(_ => import("../pages/discover/c-pages/album"));
-// const Player = React.lazy(_ => import("../pages/player"));
-
+const Friends = React.lazy((_) => import("../pages/friends/index.js"));
+const Mine = React.lazy((_) => import("../pages/mine/index"));
+const Recommend = React.lazy((_) =>
+  import("../pages/discover/c-pages/recommend")
+);
+const Ranking = React.lazy((_) => import("../pages/discover/c-pages/ranking"));
+const Songs = React.lazy((_) => import("../pages/discover/c-pages/songs"));
+const Djradio = React.lazy((_) => import("../pages/discover/c-pages/djradio"));
+const Artist = React.lazy((_) => import("../pages/discover/c-pages/artist"));
+const Album = React.lazy((_) => import("../pages/discover/c-pages/album"));
+const Player = React.lazy((_) => import("../pages/player"));
 
 const routes = [
-    {
-        path: '/',
+  {
+    path: "/",
+    exact: true,
+    render: () => <Redirect to="/discover"></Redirect>,
+  },
+  {
+    path: "/discover",
+    component: Discover,
+    routes: [
+      {
+        path: "/discover",
         exact: true,
-        render: () => (
-            <Redirect to='/discover'></Redirect>
-        )
-    },
-    {
-        path: '/discover',
-        component: Discover,
-        routes: [
-            {
-              path: "/discover",
-              exact: true,
-              render: () => (
-                <Redirect to={"/discover/recommend"}/>
-              )
-            },
-            {
-              path: "/discover/recommend",
-              component: Recommend
-            },
-            {
-              path: "/discover/ranking",
-              component: Ranking
-            },
-            {
-              path: "/discover/songs",
-              component: Songs
-            },
-            {
-              path: "/discover/djradio",
-              exact: true,
-              component: Djradio
-            },
-            {
-              path: "/discover/artist",
-              component: Artist
-            },
-            {
-              path: "/discover/album",
-              component: Album
-            },
-            {
-              path: "/discover/player",
-              component: Player
-            }
-          ]
-    },
-    {
-        path: '/mine',
-        component: Mine
-    },
-    {
-        path: '/friend',
-        component: Friends
-    },
+        render: () => <Redirect to={"/discover/recommend"} />,
+      },
+      {
+        path: "/discover/recommend",
+        component: Recommend,
+      },
+      {
+        path: "/discover/ranking",
+        component: Ranking,
+      },
+      {
+        path: "/discover/songs",
+        component: Songs,
+      },
+      {
+        path: "/discover/djradio",
+        exact: true,
+        component: Djradio,
+      },
+      {
+        path: "/discover/artist",
+        component: Artist,
+      },
+      {
+        path: "/discover/album",
+        component: Album,
+      },
+      {
+        path: "/discover/player",
+        component: Player,
+      },
+    ],
+  },
+  {
+    path: "/mine",
+    component: Mine,
+  },
+  {
+    path: "/friend",
+    component: Friends,
+  },
+];
 
-]
-
-export default routes
+export default routes;
